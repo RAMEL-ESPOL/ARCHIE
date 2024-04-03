c83137f38714f4d63c53c212e5955f07a8c9adde
M4V3R1CK100 <eridamen@espol.edu.ec>
2024-04-03 09:02:22 -0500

    cambie el control del pid

--- communication/src/u2d2_communication.py
+++ communication/src/u2d2_communication.py
@@ -104,9 +104,9 @@ if __name__ == '__main__':
 
     #Last value is the max desired speed: value*0.229rpm is the speed in rpm
     print(dxl_baud_rate)
-    base = XCseries_motor(usb_port,dxl_baud_rate,[0,1],portHandler,packetHandler,r,15,{0:[-1.57,1.57],1:[-0.785,0.785]},{0:[780,100,2000],1:[780,100,2000]})
-    codo = XCseries_motor(usb_port,dxl_baud_rate,[2,3],portHandler,packetHandler,r,15,{2:[-1.15,2],3:[-3.14,3.14]},{2:[780,100,2000],3:[780,100,2000]})
-    ee   = XCseries_motor(usb_port,dxl_baud_rate,[4,5],portHandler,packetHandler,r,15,{4:[-1.15,2],5:[-3.14,3.14]},{4:[780,100,2000],5:[780,100,2000]})
+    base = XCseries_motor(usb_port,dxl_baud_rate,[0,1],portHandler,packetHandler,r,15,{0:[-1.57,1.57],1:[-0.785,0.785]},{0:[780,100,2000],1:[800,100,2000]})
+    codo = XCseries_motor(usb_port,dxl_baud_rate,[2,3],portHandler,packetHandler,r,15,{2:[-1.15,2],3:[-3.14,3.14]},{2:[800,100,2000],3:[640,0,1000]})
+    ee   = XCseries_motor(usb_port,dxl_baud_rate,[4,5],portHandler,packetHandler,r,15,{4:[-1.15,2],5:[-3.14,3.14]},{4:[640,0,1000],5:[640,0,1000]})
 
     list_motors = [base,codo,ee]
 
@@ -120,6 +120,17 @@ if __name__ == '__main__':
     print("subcribir")
 
     while not rospy.is_shutdown():  
+        if ({0:[780,100,2000],1:[800,100,2000]} != base.get_pid_list):
+            rospy.logwarn("El PID de la base cambió a " + str(base.get_pid_list))
+            base.config_pid_cts({0:[780,100,2000],1:[800,100,2000]})    
+        if ({2:[800,100,2000],3:[640,0,1000]} != codo.get_pid_list):
+            rospy.logwarn("El PID del codo cambió a " + str(codo.get_pid_list))
+            codo.config_pid_cts({2:[800,100,2000],3:[640,0,1000]})
+        if ({4:[640,0,1000],5:[640,0,1000]} != ee.get_pid_list):
+            rospy.logwarn("El PID del ee cambió a " + str(ee.get_pid_list))
+            ee.config_pid_cts({4:[640,0,1000],5:[640,0,1000]})
+
+        
         #print("Antes del Spin")
         rospy.spin()
         #print("Escuchando...")
