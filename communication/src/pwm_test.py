cddf9e9cf5ad87b4f519b1b03627a5d9db3754ff
Juan Saeteros <jumasaet@espol.edu.ec>
2024-07-12 12:55:14 -0500

    Movimiento en ambas direcciones usando PWM

--- communication/src/pwm_test.py
+++ communication/src/pwm_test.py
@@ -4,20 +4,38 @@
 # https://emanual.robotis.com/docs/en/software/dynamixel/dynamixel_workbench/
 
 from dynamixel_sdk import *                    # Uses Dynamixel SDK library
+from getch import getch
+
+def convert_to_signed_16bit(val):
+    if val > 0x7FFF:
+        val -= 0x10000
+    return val
+
+def convert_to_signed_32bit(val):
+    if val > 0x7FFFFFFF:
+        val -= 0x100000000
+    return val
+
+def convert_hex(pwm):
+    if pwm < 0:
+        pwm = (1 << 16) + pwm
+    return pwm
+
 
 #Control table address
 ADDR_OPERATING_MODE         = 11               # Control table address is different in Dynamixel model
 ADDR_PRO_TORQUE_ENABLE      = 64               # Control table address is different in Dynamixel model
 ADDR_PRO_GOAL_PWM           = 100
 ADDR_PRO_PRESENT_PWM        = 124
+ADDR_PRO_PRESENT_VEL        = 128
 
 
 # Protocol version
 PROTOCOL_VERSION            = 2.0               # See which protocol version is used in the Dynamixel
 
 # Default setting
-DXL_ID                      = 1                 # Dynamixel ID : 1
-BAUDRATE                    = 57600             # Dynamixel default baudrate : 57600
+DXL_ID                      = 5                 # Dynamixel ID : 5
+BAUDRATE                    = 1000000             # Dynamixel default baudrate : 57600
 DEVICENAME                  = '/dev/ttyUSB0'           #'/dev/ttyUSB0'    # Check which port is being used on your controller
                                                 # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
 
@@ -82,9 +100,10 @@ while 1:
 
     while 1: 
         i = i+1 
-        pwm = 0.113   
+        pwm = -885 
+
         # Write goal position
-        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID, ADDR_PRO_GOAL_PWM, pwm)
+        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID, ADDR_PRO_GOAL_PWM, convert_hex(pwm))
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
@@ -96,7 +115,9 @@ while 1:
         elif dxl_error != 0:
             print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-        print("[ID:%03d] GoalPwm:%03d  PresPwm:%03d" % (DXL_ID, pwm, dxl_present_pwm))
+        dxl_present_vel, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, DXL_ID, ADDR_PRO_PRESENT_VEL)
+
+        print("[ID:%03d] GoalPwm:%03d  PresPwm:%03d  PresVel:%03d" % (DXL_ID, pwm, convert_to_signed_16bit(dxl_present_pwm), convert_to_signed_32bit(dxl_present_vel)))
             
         if i ==50:
             i=0
