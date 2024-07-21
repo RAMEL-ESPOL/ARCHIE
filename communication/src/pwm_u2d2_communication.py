fcd2ccfe03b0d455c15921425021ac2c4511bb94
Juan Saeteros <jumasaet@espol.edu.ec>
2024-07-21 16:26:11 -0500

    Changes made for pwm control mode calculating the necessary torque

--- communication/src/pwm_u2d2_communication.py
+++ communication/src/pwm_u2d2_communication.py
@@ -10,6 +10,8 @@ from sensor_msgs.msg import JointState
 from std_msgs.msg import Header
 from dynamixel_sdk import * 
 from getch import getch
+import numpy as np
+import moveit_commander
 
 ADDR_OPERATING_MODE         = 11               # Control table address is different in Dynamixel model
 ADDR_PRO_TORQUE_ENABLE      = 64               # Control table address is different in Dynamixel model
@@ -37,77 +39,101 @@ def convert_hex(pwm):
         pwm = (1 << 16) + pwm
     return pwm
 
+def motor_angle_to_radian(self, motor_angle_val):
 
-def set_sync_positions(data : JointState,callback_args):
-    list_motors = callback_args[0]
-    bool_init   = callback_args[1]
-    print("""=====================================================================================Lista de motores""")
-    for motor in list_motors:
-        for id in motor.list_ids:
-            print("ID Motor: " + str(id))
-            #Check if zero value positions are the initial positions for each joint!!!!!!!!!!!!!!!!!!!!!!!!
-            if bool_init:
-                param_goal_position = [DXL_LOBYTE(DXL_LOWORD(2048)), DXL_HIBYTE(DXL_LOWORD(2048)), DXL_LOBYTE(DXL_HIWORD(2048)), DXL_HIBYTE(DXL_HIWORD(2048))]
-                # Add Dynamixel#1 goal position value to the Syncwrite parameter storage
-                dxl_addparam_result = groupSyncWrite.addParam(id, param_goal_position)
-                if dxl_addparam_result != True:
-                    print("[ID:%03d] groupSyncWrite addparam failed" % id)
-                print("Dynamixel has successfully set the initial position")
+    min_value_angle = 0
+    max_value_angle = 4095 
+    min_angle_deg = 0
+    max_angle_deg = 360
 
-            else:
-                new_angle = motor.angleConversion(data.position[id], False,id) 
-                param_goal_position = [DXL_LOBYTE(DXL_LOWORD(new_angle)), DXL_HIBYTE(DXL_LOWORD(new_angle)), DXL_LOBYTE(DXL_HIWORD(new_angle)), DXL_HIBYTE(DXL_HIWORD(new_angle))]
-                # Add Dynamixel#1 goal position value to the Syncwrite parameter storage
-                dxl_addparam_result = groupSyncWrite.addParam(id, param_goal_position)
-                if dxl_addparam_result != True:
-                    print("[ID:%03d] groupSyncWrite addparam failed" % id)
-                print("The new angle is " + str(new_angle*180/2048))
+    zero_value_angle = (max_value_angle-min_value_angle)//2 + 1
+    deg_movement_span_2 = (max_angle_deg-min_angle_deg)//2
+    rad_movement_span_2 = float(deg_movement_span_2*(pi/180))        
 
-    dxl_comm_result = groupSyncWrite.txPacket()
-    if dxl_comm_result != COMM_SUCCESS:
-        print("Hubo un fallo")
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    if (motor_angle_val == zero_value_angle):
+        return 0.0
+    else:
+        return ((float)(motor_angle_val-zero_value_angle)/(float)(zero_value_angle))*rad_movement_span_2
 
-    # Clear syncwrite parameter storage
-    groupSyncWrite.clearParam()
-
-    print("=====================================================================================")
-
-
-def get_positions(list_motors):# Read present position
+def get_positions():# Read present position
     present_positions = [0,0,0,0,0,0]   
     # Syncread present position
     dxl_comm_result = groupSyncReadPos.txRxPacket()
     if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+        print("Falla en get_positions: %s" % packetHandler.getTxRxResult(dxl_comm_result))
     
-    for motor in list_motors:
-        for id in motor.list_ids:
-            # Check if groupsyncread data of Dynamixel#1 is available
-            dxl_getdata_result = groupSyncReadPos.isAvailable(id, motor.addr_present_position , 4)
-            if dxl_getdata_result != True:
-                print("[ID:%03d] groupSyncRead getdata failed" % id)
-            # Get Dynamixel present position value
-            dxl_present_position = groupSyncReadPos.getData(id, motor.addr_present_position , 4)
-            present_positions[id]=dxl_present_position
+    for id in range(6):
+        # Check if groupsyncread data of Dynamixel#1 is available
+        dxl_getdata_result = groupSyncReadPos.isAvailable(id, ADDR_PRO_PRESENT_POS , 4)
+        if dxl_getdata_result != True:
+            print("[ID:%03d] groupSyncReadPos getdata failed" % id)
+        # Get Dynamixel present position value
+        dxl_present_position = motor_angle_to_radian(groupSyncReadPos.getData(id, ADDR_PRO_PRESENT_POS , 4))
+        present_positions[id]=dxl_present_position
+
     return present_positions
 
 
-def joint_state_publisher(list_motors,num_joints):
+def calculate_torque(joints: JointState):
+    jac_t = np.transpose(group.get_jacobian_matrix(list(np.array(joints.position,float))))
+    gravity = -9.81
+    mass_vector = np.transpose(np.array([0.102, 0.101, 0.102, 0.105, 0.102, 0.05],float))
+    gravity_force = mass_vector * gravity
+    gravity_compensation = np.dot(jac_t, gravity_force)
+
+    return gravity_compensation
+
+def move_to_target(goal_position: JointState):
+
+    motor_positions = get_positions()
+    current_positions = list(np.array(motor_positions, float))
+
+    # Calcula los torques de gravedad para la posición actual
+    gravity_torques = calculate_torque(current_positions)
+
+    # Calcula los torques adicionales necesarios para moverse hacia la posición objetivo
+    position_error = np.array(goal_position) - np.array(current_positions)
+    k_p = 10  # Ganancia proporcional (ajusta según sea necesario)
+    position_torques = k_p * position_error
+
+    effort = (gravity_torques + position_torques)
+
+    joint_state_publisher(motor_positions, effort)
+    set_sync_pwm(effort)
+
+        
+
+def set_sync_pwm(total_torques):
+    print("""=====================================================================================Lista de motores""")
+
+    for id in range(6):
+        new_pwm = convert_hex(total_torques[id]*885/1.2)
+        param_goal_position = [DXL_LOBYTE(DXL_LOWORD(new_pwm)), DXL_HIBYTE(DXL_LOWORD(new_pwm)), DXL_LOBYTE(DXL_HIWORD(new_pwm)), DXL_HIBYTE(DXL_HIWORD(new_pwm))]
+        # Add Dynamixel#1 goal position value to the Syncwrite parameter storage
+        dxl_addparam_result = groupSyncWritePWM.addParam(id, param_goal_position)
+        if dxl_addparam_result != True:
+            print("[ID:%03d] groupSyncWritePWM addparam failed" % id)
+
+    dxl_comm_result = groupSyncWritePWM.txPacket()
+    if dxl_comm_result != COMM_SUCCESS:
+        print("Hubo un fallo en envío de PWM")
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+
+    # Clear syncwrite parameter storage
+    groupSyncWritePWM.clearParam()
+
+    print("=====================================================================================")
+
+
+def joint_state_publisher(current_positions, efforts):
     joints_states = JointState()
     joints_states.header = Header()
     joints_states.header.stamp = rospy.Time.now()
-    joints_states.name = ['joint_'+str(id+1) for id in range(num_joints)]
-    #Read actual motor state after movement occured
-    general_joint_position = get_positions(list_motors)
-    #Convert from 0-4095 to degrees
-    #print("Joint State")
-    if general_joint_position != general_joint_position_state:
-        for motor in list_motors:
-            for id in motor.list_ids:
-                general_joint_position_state[id]=motor.angleConversion(general_joint_position[id],True,id) 
+    joints_states.name = ['joint_'+str(id+1) for id in range(6)]
+    
     #Publish the new joint state
-    joints_states.position = general_joint_position_state
+    joints_states.position = current_positions
+    joints_states.effort = efforts
     joint_state_pub.publish(joints_states)
 
 
@@ -119,34 +145,27 @@ if __name__ == '__main__':
     usb_port = rospy.get_param('~usb_port')
     dxl_baud_rate = rospy.get_param('~dxl_baud_rate')
 
-
-    num_joints = 6
-    general_joint_position = [0 for i in range(num_joints)]
-    general_joint_position_state = [0 for i in range(num_joints)]
-
     portHandler = PortHandler(usb_port)
     packetHandler = PacketHandler(2.0)
 
-    # Set operating mode to extended position control mode
-    for id in range(6):
+    # Set operating mode to pwm control mode
+    for id in range(6):     
         dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, id, ADDR_OPERATING_MODE, PWM_CONTROL_MODE)
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
             print("%s" % packetHandler.getRxPacketError(dxl_error))
         else:
-            print("Operating mode changed to extended position control mode.")
+            print("Operating mode for motor", str(id), " changed to PWM control mode.")
 
-    # Enable Dynamixel Torque
     for id in range(6):
         dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, id, ADDR_PRO_TORQUE_ENABLE, TORQUE_ENABLE)
-
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
         elif dxl_error != 0:
             print("%s" % packetHandler.getRxPacketError(dxl_error))
         else:
-            print("Dynamixel Motor ",id, " has been successfully connected")
+            print("Torque of Motor ",id," is on")
 
     # Initialize GroupSyncWrite/Read instance
     groupSyncWritePWM       = GroupSyncWrite(portHandler, packetHandler, ADDR_PRO_GOAL_PWM, 4)
@@ -164,18 +183,12 @@ if __name__ == '__main__':
 
     #Publish current robot state
     joint_state_pub = rospy.Publisher('/real_joint_states', JointState, queue_size=10)
+    subGoalState    = rospy.Subscriber('/joint_goals', JointState, callback = move_to_target, queue_size= 5)
+    group           = moveit_commander.MoveGroupCommander("arm_group")
+    
+    rospy.spin()    
 
-    set_sync_positions({},[list_motors, True])
-
-    # Subscribe desired joint position
-    rospy.Subscriber('/joint_goals', JointState,set_sync_positions,(list_motors,False),queue_size= 5)
-
-    while not rospy.is_shutdown():
-        #se lama a la funcion joint state publisher para publicar los /joint_states a ROS
-        joint_state_publisher(list_motors, num_joints)
-        r.sleep()
-        
     # Clear syncread parameter storage
-    groupSyncRead.clearParam()
+    groupSyncReadPos.clearParam()
 
     portHandler.closePort()
