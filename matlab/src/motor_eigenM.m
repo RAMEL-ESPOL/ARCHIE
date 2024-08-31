9d4b0985352fd47882643e4e4fd0fe0f3249ccca
Juan Saeteros <jumasaet@espol.edu.ec>
2024-08-31 14:25:17 -0500

    Matlab file modified to get the mass eigen and damp values wished, also urdf files modified to get a better mass matrix, still there are changes to make

--- matlab/src/motor_eigenM.m
+++ matlab/src/motor_eigenM.m
@@ -1,22 +1,18 @@
-robot = importrobot('archie_description\urdf\manipulator.urdf');
+robot = importrobot('archie_description\urdf\manipulator2.urdf');
 robot.DataFormat = 'row';
 robot.Gravity = [0 0 -9.81];
 
 jointVal = [0 0 0 0 0 0];
 
 
-k_p = [3 3.5 2.5 2 2 2];
-c_p = [0.2 0.6 0.15 0.4 0.1 0.1];
+k_p = eye(6,6).*[3 3.5 2.5 2 2 2]
+c_p = eye(6,6).*[0.2 0.6 0.15 0.4 0.1 0.1]
 
-M = massMatrix(robot, jointVal);
+M = massMatrix(robot, jointVal)
 M_inv = inv(M)
-MK = massMatrix(robot, k_p);
-MC = massMatrix(robot, c_p);
-MK_inv = inv(MK);
-MC_inv = inv(MC);
 
 A = [zeros(6,6), eye(6,6); %         Zeros(6x6), I(6x6)
-     M_inv.*(-k_p),  M_inv.*(-c_p)];%-M_inv*K  , -M_inv*C 
+     M_inv*(-k_p),  M_inv*(-c_p)];%-M_inv*K  , -M_inv*C 
 
 eigen_val = eig(A)
 
