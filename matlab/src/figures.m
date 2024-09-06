4d816543da1e2c0fd204322a316bb6835f47eccd
Juan Saeteros <jumasaet@espol.edu.ec>
2024-09-06 16:55:50 -0500

    from m to mm

--- matlab/src/figures.m
+++ matlab/src/figures.m
@@ -3,15 +3,15 @@ robot.DataFormat = 'row';
 robot.Gravity = [0 0 -9.81];
 
 ndoc = 11;
+% 
+% joint_goals   = deg2rad(table2array(readtable('matlab/data/joint_goals_square_t5_h30_p22.txt')));
+% joint_states  = deg2rad(table2array(readtable('matlab/data/joint_real_states_square_t5_h30_p22.txt')));
 
-joint_goals   = deg2rad(table2array(readtable('matlab/data/joint_goals_square_t5_h25_p22.txt')));
-joint_states  = deg2rad(table2array(readtable('matlab/data/joint_real_states_square_t5_h25_p22.txt')));
-
-% joint_goals   = deg2rad(table2array(readtable('matlab/data/joint_goals_square_t5_h20_p15.txt')));
+% joint_goals   = deg2rad(table2array(readtable('matlab/data/joint_goals_square_t5_h_p15.txt')));
 % joint_states  = deg2rad(table2array(readtable('matlab/data/joint_real_states_square_t5_h20_p15.txt')));
     
-% joint_goals   = deg2rad(table2array(readtable('matlab/data/joint_goals_circle_t0_h30_p22.txt')));
-% joint_states  = deg2rad(table2array(readtable('matlab/data/joint_real_states_circle_t0_h30_p22.txt')));
+joint_goals   = deg2rad(table2array(readtable('matlab/data/joint_goals_circle_t0_h30_p22.txt')));
+joint_states  = deg2rad(table2array(readtable('matlab/data/joint_real_states_circle_t0_h30_p22.txt')));
 
 % joint_goals   = deg2rad(table2array(readtable('matlab/data/joint_goals_triangle_t0_h30_p22.txt')));
 % joint_states  = deg2rad(table2array(readtable('matlab/data/joint_real_states_triangle_t0_h30_p22.txt')));
@@ -21,29 +21,29 @@ cart_goals = [];
 
 for i=1:length(joint_goals)
     tform_states = getTransform(robot, joint_states(i, : ), 'link_6'); 
-    pos_states = tform2trvec(tform_states);
+    pos_states = tform2trvec(tform_states)*1000;
     cart_states = [cart_states; pos_states];
     
     tform_goals = getTransform(robot, joint_goals(i, : ), 'link_6'); 
-    pos_goals = tform2trvec(tform_goals);
+    pos_goals = tform2trvec(tform_goals)*1000;
     cart_goals = [cart_goals; pos_goals];
 end
 
 % Aplicar condición para Z y eliminar filas donde Z >= 0.27
-cond_goals = cart_goals(:, 3) < 0.25; % Puntos en los que escribe
+cond_goals = cart_goals(:, 3) < 222; % Puntos en los que escribe
 cart_goals = cart_goals(cond_goals, :); % Filtrar filas completas
 
-cond_states = cart_states(:, 3) < 0.25; % Puntos en los que escribe
+cond_states = cart_states(:, 3) < 222; % Puntos en los que escribe
 cart_states = cart_states(cond_states, :); % Filtrar filas completas
 
 colors = lines(3); % Define a set of colors
 
 figure(2);
 plot(cart_states(:,1), cart_states(:,2),"LineWidth",2, 'Color', colors(1,:))
-grid on
-grid minor
-xlim([-0.05, 0.05])
-ylim([0.20, 0.25])
+xlim([-85, 85])
+ylim([215, 305])
+ylabel('Y (mm)', 'FontSize', 12);
+xlabel('X (mm)', 'FontSize', 12);
 
 hold on
 plot(cart_goals(:,1), cart_goals(:,2),"LineWidth",2, 'Color', colors(2,:))
