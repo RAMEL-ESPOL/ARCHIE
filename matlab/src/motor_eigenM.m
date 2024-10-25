96db147824e8b02e3294460cc3ec7b6700b1c816
Juan Saeteros <jumasaet@espol.edu.ec>
2024-10-25 13:59:12 -0500

    Pequeños cambios en la función para ver las frecuencias y damping

--- matlab/src/motor_eigenM.m
+++ matlab/src/motor_eigenM.m
@@ -5,10 +5,10 @@ robot.Gravity = [0 0 -9.81];
 n = 6;
 jointVal = rand(1,n)
 
-% jointVal = [0,0,0,0,0,0];
+jointVal = [0,0,0,0,0,0];
 
 k_p = eye(n,n).*[3 3.5 2.5 2 2 2];
-% c_p = eye(n,n).*[0.2 0.6 0.15 0.4 0.1 0.1]; Antiguo
+% c_p = eye(n,n).*[0.2 0.6 0.15 0.4 0.1 0.1]; %Antiguo
 c_p = eye(n,n).*[0.1740    0.5546    0.2957    0.0673    0.0595    0.0023];
 
 M = massMatrix(robot, jointVal);
@@ -27,5 +27,4 @@ for i=1:length(eigen_val)
     damp = [damp; cos(angle(eigen_val(i)))];
 end
 
-wn
-damp
+[wn damp*10000]
