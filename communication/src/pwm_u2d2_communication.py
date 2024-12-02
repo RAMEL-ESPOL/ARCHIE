c8468208eea268a65a85d50c85bc8f605ca1f0d2
Juan Saeteros <jumasaet@espol.edu.ec>
2024-12-02 03:49:28 -0500

    Falta revisar por qué la frecuencia sigue siendo 20 aunque recibe topics de hasta 50 Hz

--- communication/src/pwm_u2d2_communication.py
+++ communication/src/pwm_u2d2_communication.py
@@ -214,7 +214,7 @@ def move_to_target(state_position: JointState):
     k_p = pid_array[n_pid][0]
     c_p = pid_array[n_pid][1]
 
-    print(current_time)
+    rospy.logwarn(current_time)
 
     error_torques = (position_error*k_p)
     damp_torques  = ((np.array(motor_velocities, float)*(2*math.pi/60))*c_p) #primero convertimos vel a rad/s
@@ -249,10 +249,10 @@ def move_to_target(state_position: JointState):
     joint_state_publisher(motor_positions, motor_velocities, motor_efforts)
     set_sync_pwm(np.array(total_pwm))
 
-    rospy.logwarn(f"PWM: {total_pwm}")
-    rospy.logwarn(f"Vel: {motor_velocities}")
-    rospy.logwarn(f"Par: {motor_efforts}")
-    rospy.logwarn(f"Err: {position_error*180/math.pi}")
+    # rospy.logwarn(f"PWM: {total_pwm}")
+    # rospy.logwarn(f"Vel: {motor_velocities}")
+    # rospy.logwarn(f"Par: {motor_efforts}")
+    # rospy.logwarn(f"Err: {position_error*180/math.pi}")
     
     motor = MotorData()
     motor.position = motor_positions
@@ -293,13 +293,13 @@ if __name__ == '__main__':
 
     # Default setting
     DXL_ID                      = 5                 # Dynamixel ID : 5
-    BAUDRATE                    = 1000000             # Dynamixel default baudrate : 57600
-    DEVICENAME                  = '/dev/ttyUSB0'           #'/dev/ttyUSB0'    # Check which port is being used on your controller
+    BAUDRATE                    = 1000000           # Dynamixel default baudrate : 57600
+    DEVICENAME                  = '/dev/ttyUSB0'    #'/dev/ttyUSB0'    # Check which port is being used on your controller
                                                     # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
 
     TORQUE_ENABLE               = 1                 # Value for enabling the torque
     TORQUE_DISABLE              = 0                 # Value for disabling the torque
-    PWM_CONTROL_MODE            = 16                         # Value for extended position control mode (operating mode)
+    PWM_CONTROL_MODE            = 16                # Value for extended position control mode (operating mode)
     POS_CONTROL_MODE            = 3
 
     index = 0
@@ -348,7 +348,7 @@ if __name__ == '__main__':
 
 
     rospy.init_node("motor_communication_pwm")
-    r =rospy.Rate(10) # 10hz
+    r =rospy.Rate(30) # 10hz
 
     # Set operating mode to pwm control mode
     for id in range(NUM_MOTORS):     
@@ -374,9 +374,18 @@ if __name__ == '__main__':
     #Publish current robot state
     joint_state_pub = rospy.Publisher('/real_joint_states', JointState, queue_size=10)
     motor_state_pub = rospy.Publisher ("/motor_data" , MotorData, queue_size=1)
-    subGoalState    = rospy.Subscriber('/joint_goals', JointState, callback = move_to_target, queue_size= 5)
+    subGoalState    = rospy.Subscriber('/joint_states', JointState, callback = move_to_target, queue_size= 5)
     
-    rospy.spin()    
+    rospy.spin()
+    # while not rospy.is_shutdown():
+        # current_time = time.time() - start_time
+        
+        # try:
+        #     r.sleep()
+        #     rospy.logerr(current_time)
+        # except rospy.ROSInterruptException as e:
+        #     print("Sleep interrupted:", e)
+
 
     # Disable the torque
     for id in range(NUM_MOTORS):
