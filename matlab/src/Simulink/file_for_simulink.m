e690a1829c8b89c2d18cb5d471421147ebe55896
Juan Saeteros <jumasaet@espol.edu.ec>
2025-01-08 14:18:51 -0500

    .

--- matlab/src/Simulink/file_for_simulink.m
+++ matlab/src/Simulink/file_for_simulink.m
@@ -1,19 +1,24 @@
-joint_goals   = deg2rad(table2array(readtable('matlab/data/joint_goals_square_t5_h30_p22.txt')));
+ndoc  = 21;
+n_pid = 5;
+frec  = 10;
+
+joint_error_real  = importdata(strcat('matlab/data_pwm/',num2str(ndoc),'_motor_error.txt'));joint_states_real = importdata(strcat('matlab/data_pwm/',num2str(ndoc),'_motor_position.txt'));joint_goals = joint_error_real + joint_states_real;
+
+joint_torque_real = importdata(strcat('matlab/data_pwm/',num2str(ndoc),'_motor_effort.txt'));
+
+time_real= linspace(1/frec, length(joint_goals)/frec, length(joint_goals));
 
 simu_decimation = 1;
-step_time = 0.1;
-stop_time = 3;
-n_pid = 6;
+step_time = 1/frec;
+stop_time = time_real(length(time_real));
 
-% joint_goals = zeros(30000, 6);
-joint_goals_step = [-1.57 1.57/2 -1.57/2 1.57/3 1.57 1.57];
-input_data = [(linspace(0, stop_time, length(joint_goals)))', joint_goals];
+input_data = [time_real', joint_goals];
 
 robot = importrobot('archie_description\urdf\manipulator.urdf');
 robot.DataFormat = 'row';
 robot.Gravity = [0 0 -9.81];
 
-% Con esto únicamente debemos cambiar la variable ndoc para usar otro array de PID
+% Con esto únicamente debemos cambiar la variable n_pid para usar otro array de PID
 % lo que nos evita tener que andar comentando y/o descomentando código
 pid_array(:,:,1) = [
     3 3.5 2.5 2 2 0;
@@ -47,11 +52,56 @@ pid_array(:,:,6) = [
     0 0 0 0 0 0
 ];
 
-
-
 k_p = pid_array(1,:,n_pid);
 c_p = pid_array(2,:,n_pid);
 i_p = pid_array(3,:,n_pid);
 
-
 out = sim("matlab\src\Simulink\manipulator_pid_torque", 'StopTime', num2str(stop_time));
+
+joint_states_simu = out.joint_states; joint_states_simu = joint_states_simu(1: length(joint_states_simu)-1, :);
+joint_error_simu  = (joint_goals - joint_states_simu); % Convertimos a grados
+
+method = "traj";
+
+joint_error_simu  = rad2deg(joint_error_simu);
+joint_error_real  = rad2deg(joint_error_real);
+joint_goals       = rad2deg(joint_goals); 
+joint_states_simu = rad2deg(joint_states_simu);
+joint_states_real = rad2deg(joint_states_real);
+
+colors = lines(7);
+set(groot, 'defaultFigureWindowState', 'maximized')
+
+figure(); sgtitle(strcat('Error for each joint (PID', num2str(n_pid), ') @', num2str(frec), 'Hz'));
+for i=1:6   
+    subplot(3, 2, i); plot(time_real, joint_error_simu(:, i),"LineWidth",1, 'Color', colors(2,:)); hold on;
+    subplot(3, 2, i); plot(time_real, joint_error_real(:, i),"LineWidth",1, 'Color', colors(3,:)); 
+    legend(strcat("Simu joint ", num2str(i), ", Mean Error: ", num2str(round(mean(abs(joint_error_simu(:, i))), 3))), strcat("Real joint ", num2str(i), ", Mean Error: ", num2str(round(mean(abs(joint_error_real(:, i))), 3))), "Location", "best"); grid minor;
+    set(gca,'FontSize',10); xlim([0 time_real(length(time_real))]);
+end
+xlabel('Time(seconds)', 'FontSize', 12); ylabel('Joint Error (degrees)', 'FontSize', 12); set(gca,'FontSize',10); 
+exportgraphics(gcf, strcat(num2str(frec),"_",method, "_PID", num2str(n_pid), "_f",num2str(frec), "_",  "_error.png"), "Resolution", 300)
+
+
+figure();sgtitle(strcat('Input/Output Response (PID', num2str(n_pid), ') @', num2str(frec), 'Hz'));
+for i=1:6   
+    subplot(3, 2, i); plot(time_real, joint_goals(:, i),"LineWidth",1, 'Color', colors(1,:)); hold on;
+    subplot(3, 2, i); plot(time_real, joint_states_simu(:, i),"LineWidth",1, 'Color', colors(2,:)); hold on;
+    subplot(3, 2, i); plot(time_real, joint_states_real(:, i),"LineWidth",1, 'Color', colors(3,:)); 
+    legend("Input", "Simu Output", "Real Output", "Location", "best"); grid minor;
+    set(gca,'FontSize',10); xlim([0 time_real(length(time_real))]);
+end
+xlabel('Time(seconds)', 'FontSize', 12); ylabel('Joint Position (degrees)', 'FontSize', 12); set(gca,'FontSize',10); 
+exportgraphics(gcf, strcat(num2str(frec),"_",method, "_PID", num2str(n_pid), "_f",num2str(frec), "_",  "_in_out.png"), "Resolution", 300)
+
+joint_torque = out.joint_torques;joint_torque = joint_torque(1: length(joint_torque)-1, :);
+figure(); sgtitle(strcat("Torque for each joint (PID", num2str(n_pid), ') @', num2str(frec), 'Hz'));
+for i=1:6   
+    subplot(3, 2, i); plot(time_real, joint_torque(:, i),"LineWidth",1, 'Color', colors(2,:)); hold on;
+    subplot(3, 2, i); plot(time_real, joint_torque_real(:, i),"LineWidth",1, 'Color', colors(3,:)); 
+    legend(strcat("Simu torque", num2str(i)), strcat("Real torque", num2str(i)), "Location", "best"); grid minor;
+    set(gca,'FontSize',10); xlim([0 time_real(length(time_real))]);
+end
+xlabel('Time(seconds)', 'FontSize', 12); ylabel('Joint Torques (Nm)', 'FontSize', 12); set(gca,'FontSize',10);
+exportgraphics(gcf, strcat(method, "_PID", num2str(n_pid), "_f",num2str(frec), "_",  "_torque.png"), "Resolution", 300)
+
